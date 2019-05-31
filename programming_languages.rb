d6b05f49b0e9ea52e20cd44211ba0073a3ed08c8
layla dantas <laylatdantas@gmail.com>
2019-05-31 04:24:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  
+  new_hash = {}
+  
+  languages.each do |k,v|
+    v.each do |k1,v1|
+      new_hash[k1] = v1
+      puts k1
+      puts new_hash
+      unless new_hash[k1].keys.include?(:style)
+         new_hash[k1][:style] = []
+      end
+      unless new_hash[k1][:style].include?(k)
+        new_hash[k1][:style] << k
+      end
+      
+    end 
+ end
+ new_hash
 end
+
+
+
+#hash[:my_key] = {second_level_key: "second level value"}
